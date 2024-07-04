a13cfdddec3af8ec35d481e28bc48cedd9604a8a
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-07-04 14:44:21 +0500

    Add few changes on mobile responsive

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -10,10 +10,10 @@ const Services = () => {
             </div>
             <div className='services-container grid grid-cols-2 lg:grid-cols-3 gap-3  lg:gap-10 mb-10'>
                 {ServicesData.map((service, index) => (
-                    <div key={index} className="services-format flex flex-col justify-center gap-5 p-14 border border-white rounded-lg transition-all cursor-pointer">
-                        <h3 className='font-semibold text-3xl'>{service.serviceNo}</h3>
-                        <h2 className='text-4xl font-extrabold'>{service.serviceName}</h2>
-                        <p>{service.serviceDesc}</p>
+                    <div key={index} className="services-format flex flex-col justify-center gap-5 p-3 lg:p-14 border border-white rounded-lg transition-all cursor-pointer">
+                        <h3 className='font-semibold lg:text-3xl'>{service.serviceNo}</h3>
+                        <h2 className='lg:text-4xl font-extrabold'>{service.serviceName}</h2>
+                        <p className='text-sm lg:text-xl'>{service.serviceDesc}</p>
                         <div className="services-readmore flex items-center gap-5">
                             <p>Read More</p>
                             <FaArrowRightLong />
