b7470eea55327b0493b11ca0694a0e2dba7e151f
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-07-04 19:28:36 +0500

    fixed bugs in code

--- src/components/Mywork/Mywork.jsx
+++ src/components/Mywork/Mywork.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { workData } from './workData';
 import { FaArrowRightLong } from "react-icons/fa6";
+
 const Mywork = () => {
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+
     return (
         <div id='portfolio' className='mywork flex flex-col justify-center items-center gap-20 overflow-hidden'>
             <div data-aos="fade-left" className='mywork-title'>
-                <h1 className='text-4xl font-bold '>My latest work</h1>
+                <h1 className='text-4xl font-bold'>My latest work</h1>
             </div>
-            <div data-aos="fade-left" className="mywork-container grid grid-cols-2 lg:grid-cols-3">
+            <div data-aos="fade-left" className="mywork-container grid grid-cols-2 lg:grid-cols-3 gap-10">
                 {workData.map((work, index) => {
-                    return <img key={index} src={work.w_img} alt="" />
+                    return (
+                        <div 
+                            key={index} 
+                            className={`image-container ${hoveredIndex !== null && hoveredIndex !== index ? 'blurred' : ''}`} 
+                            onMouseEnter={() => setHoveredIndex(index)} 
+                            onMouseLeave={() => setHoveredIndex(null)}
+                        >
+                            <img src={work.w_img} alt="" className={`${hoveredIndex === index ? 'zoomed' : ''}`} />
+                        </div>
+                    )
                 })}
             </div>
             <div data-aos="fade-right" className="mywork-showmore flex gap-4 border border-white items-center px-7 py-5 rounded-full cursor-pointer font-semibold text-xl mb-20 transition">
@@ -20,4 +32,4 @@ const Mywork = () => {
     )
 }
 
-export default Mywork
+export default Mywork;
